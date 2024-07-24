1d51581f64aacc3bfe31b8c072b022f7f659c09f
Nogueira <davi_nogueira@ufms.br>
2024-07-23 20:38:08 -0400

    feat : cabeçalho inicial usuário

--- mega_front/src/App.vue
+++ mega_front/src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-card class="mx-auto">
+    <v-toolbar color="green-lighten-1">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>QuackFarma</v-toolbar-title>
+      <v-text-field
+          variant="solo"
+          hide-details
+          append-inner-icon="mdi-magnify"
+      ></v-text-field>
+      <v-btn size="x-large" append-icon="mdi-account-circle">
+        Log in
+      </v-btn>
+      <v-btn size="x-large">
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-tabs align-tabs="categorias" bg-color="green-lighten-2" fixed-tabs  color="white">
+      <v-tab v-for="item in categorias" :text="item.text" :value="item.value" :key="item"/>
+    </v-tabs>
+  </v-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      categorias: [
+        {text: 'Medicamentos', value: 'medicamento'},
+        {text: 'Higiene', value: 'higiene'},
+        {text: 'Suplemento', value: 'suplemento'}
+      ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- mega_front/src/main.js
+++ mega_front/src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import * as mdiicon from '@mdi/font/css/materialdesignicons.css'
+
+import *  as fsicom from '@fortawesome/fontawesome-free/css/all.css'
+
+
+// Components
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+    mdiicon,
+    fsicom
+})
+
+createApp(App).use(vuetify).mount('#app')
