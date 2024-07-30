e6aead27c21040cba159be7ea845e9e8f5a7f06f
Nogueira <davi_nogueira@ufms.br>
2024-07-29 20:01:36 -0400

    feat: página de descrição do produto

--- mega_front/src/router/index.js
+++ mega_front/src/router/index.js
@@ -1,11 +1,17 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Index from "@/components/PagePrincipal/Index.vue";
+import ProductDescription from "@/components/produtos/ProductDescription.vue";
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Index
+    },
+    {
+        path : '/product/:id',
+        name : 'Product',
+        component: ProductDescription
     }
 ]
 
