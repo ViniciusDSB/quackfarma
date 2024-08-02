e738446647541950a3bace78ff50ec4b5bb8e654
viniciusb <vinidsb581@gmail.com>
2024-08-02 16:44:12 -0400

    fix: bug de merge resolvido em setDbStuff

--- mega_back/usefulScripts/setDbStuff.js
+++ mega_back/usefulScripts/setDbStuff.js
@@ -53,104 +53,9 @@ async function setMedications(){
       created_at TIMESTAMP,
       last_update TIMESTAMP)`);
 
-<<<<<<< HEAD
-  const isDataSaved = (await dbPool.query('SELECT EXISTS (SELECT 1 FROM medications WHERE name = $1)', ['Paracetamol'])).rows[0].exists ;
-  if(!isDataSaved){
-      const imagePath = 'https://images-americanas.b2w.io/produtos/7469283329/imagens/losartana-hidroclorotiazida-50-12-5mg-30cp/7469283329_1_large.jpg';
-      const managerId = 1
-      const medications = [
-          {
-            name: 'Amoxicilina',
-            code: 1001,
-            category: 'Antibiótico',
-            description: 'Antibiótico usado para tratar infecções bacterianas.',
-            needs_recipe: true,
-            unit_price: 25.75,
-            on_stock: 75,
-            image_path: imagePath,
-            created_at: new Date(),
-            last_update: new Date(),
-          },
-          {
-            name: 'Paracetamol',
-            code: 1002,
-            category: 'Analgésico',
-            description: 'Usado para aliviar dores e reduzir febres.',
-            needs_recipe: false,
-            unit_price: 15.50,
-            on_stock: 150,
-            image_path: imagePath,
-            created_at: new Date(),
-            last_update: new Date(),
-          },
-          {
-            name: 'Ibuprofeno',
-            code: 1003,
-            category: 'Anti-inflamatório',
-            description: 'Usado para reduzir inflamações e aliviar a dor.',
-            needs_recipe: true,
-            unit_price: 18.00,
-            on_stock: 120,
-            image_path: imagePath,
-            created_at: new Date(),
-            last_update: new Date(),
-          },
-          {
-            name: 'Omeprazol',
-            code: 1004,
-            category: 'Antiácido',
-            description: 'Usado para tratar refluxo ácido e úlceras estomacais.',
-            needs_recipe: true,
-            unit_price: 22.30,
-            on_stock: 95,
-            image_path: imagePath,
-            created_at: new Date(),
-            last_update: new Date(),
-          },
-          {
-            name: 'Loratadina',
-            code: 1005,
-            category: 'Antialérgico',
-            description: 'Usado para aliviar sintomas de alergias.',
-            needs_recipe: false,
-            unit_price: 10.25,
-            on_stock: 180,
-            image_path: imagePath,
-            created_at: new Date(),
-            last_update: new Date(),
-          },
-          {
-            name: 'Captopril',
-            code: 1006,
-            category: 'Anti-hipertensivo',
-            description: 'Usado para tratar pressão alta.',
-            needs_recipe: true,
-            unit_price: 30.00,
-            on_stock: 60,
-            image_path: imagePath,
-            created_at: new Date(),
-            last_update: new Date(),
-          },
-      ];
-    
-      for (const med of medications) {
-          await dbPool.query(`INSERT INTO medications (name, code, category, description, needs_recipe, unit_price, 
-            on_stock, manager, image_path, created_at, last_update) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
-            [med.name, med.code, med.category, med.description, med.needs_recipe, med.unit_price, med.on_stock, managerId, med.image_path, med.created_at, med.last_update]
-          );
-      }
-      console.log("Sample products ok");
-  }else{
-    console.log("Products ok");
-  }
-=======
     const isDataSaved = (await dbPool.query('SELECT EXISTS (SELECT 1 FROM medications WHERE name = $1)', ['Paracetamol'])).rows[0].exists ;
     if(!isDataSaved){
-<<<<<<< HEAD
-        const imagePath = `http://localhost:3001/uploads/medicinesImg/defaultMed.png`;
-=======
         const imagePath = `http://localhost:3001/uploads/medicines_images/defaultMed.png`;
->>>>>>> devops
         const managerId = 1
         const medications = [
             {
@@ -237,33 +142,20 @@ async function setMedications(){
     }else{
       console.log("Products ok");
     }
->>>>>>> 53d36b9 (feat: adicionei o upload de imagem no cadastro de novo medicamento)
     
 }
 
 async function setCart_item(){
-<<<<<<< HEAD
-  await dbPool.query('DROP TABLE IF EXISTS cart_item');
-  await dbPool.query(`CREATE TABLE IF NOT EXISTS cart_item(
-                    id SERIAL PRIMARY KEY,
-                    medicine_id INTEGER REFERENCES medications(code) ON DELETE CASCADE NOT NULL,
-                    sold_amount INTEGER NOT NULL,
-=======
   await dbPool.query(`CREATE TABLE IF NOT EXISTS cart_item(
                     id SERIAL PRIMARY KEY,
                     medicine_code INTEGER REFERENCES medications(code) ON DELETE CASCADE NOT NULL,
                     sold_amount INTEGER NOT NULL,
                     item_total NUMERIC(10, 2) NOT NULL,
->>>>>>> devops
                     approval_status BOOLEAN NOT NULL)`);
   console.log("Cart_item ok");
 }
 
 async function setSales(){
-<<<<<<< HEAD
-  await dbPool.query('DROP TABLE IF EXISTS sales');
-=======
->>>>>>> devops
   await dbPool.query(`CREATE TABLE IF NOT EXISTS sales(
                     id SERIAL PRIMARY KEY,
                     shopping_cart INTEGER[] NOT NULL,
