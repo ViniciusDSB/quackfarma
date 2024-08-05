28a345dd94efe863ce6f281f2fd9584bfc64e44a
Nogueira <davi_nogueira@ufms.br>
2024-08-04 21:58:33 -0400

    feat: notificações de sucesso e de erro

---
+++ mega_front/src/components/alertas/AlertMessage.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-snackbar
+        v-model="snackbar"
+        :top="true"
+        :color=this.color
+        location="top right"
+        :timeout=timeout
+    >
+      <v-card :color="this.color" outlined elevation="0">
+        <v-card-text >
+          {{ text }}
+
+        </v-card-text>
+      </v-card>
+
+
+      <template v-slot:actions>
+        <v-btn
+           icon="mdi-close"
+            @click="snackbar = false"
+        />
+      </template>
+    </v-snackbar>
+</template>
+<script>
+export default {
+  name : 'AlertMessage',
+  methods : {
+    message(message){
+      this.snackbar = true;
+      this.text = message
+    },
+    sucess(message){
+      this.color = 'success';
+      this.timeout = 5000;
+      this.message(message)
+    },
+    error(message){
+      this.color = 'error';
+      this.timeout = 7500;
+      this.message(message)
+    }
+
+  },
+  data: () => ({
+    snackbar: false,
+    text: `Obrigado por usar nossos sistemas`,
+    color : 'primary',
+    timeout : '5000',
+  }),
+
+}
+</script>
